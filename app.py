79fe9e01620e678aa2e4683ae5136cb483f1d010
issac Moto <ussac_123@gmail.com>
2024-09-03 18:21:58 -0600

    Se realiza el widget control

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
 st.header('Lanzar una moneda')
+# Widget control deslizante para seleccionar el número de intentos
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 
+# Botón para iniciar el experimento
+start_button = st.button('Ejecutar')
+
+# Lógica para ejecutar cuando se presiona el botón
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    
 st.write('Esta aplicación aún no es funcional. En construcción.')
